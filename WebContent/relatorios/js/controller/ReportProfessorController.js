34b3cc0df29735d327cd8fa883766de84b8144d4
Ricardo Rodriguez <ricardo.domingos@gmail.com>
2016-04-16 17:19:52 -0300

    Corrigido o elemento MockDataModel. Liberado

--- WebContent/relatorios/js/controller/ReportProfessorController.js
+++ WebContent/relatorios/js/controller/ReportProfessorController.js
@@ -3,129 +3,155 @@
  * ==============================
  */
 
-var ReportProfessorController = 
-	( function ($){
-
-		/**
-		 *  Limpa o elemento de tela informado no parametro element.
-		 *  element - elemento que será removido da tela.
-		 */
-		var doClearElement = function(element){
-			$(element).empty();
-		};
-		/**
-		 * Carrega elementos do relatório e cria um paragrafo no local informado
-		 */
-		var doRelatarMatrizAdjacencia = function(element){
-			var relatorio = $(element);
-			var paragrafo = $(document.createElement("p"));
-			var node = $(document.createTextNode("Matriz de Adjacência:"));
-			paragrafo.appendChild(node);
-			relatorio.appendChild(paragrafo);
-			
-			/*
-			 * Manual do dynatable: 
-			 *     https://www.dynatable.com/?sorts%5Byear%5D=-1#existing-json
-			 */
-			var $records = $('#json-records'),
-		    myRecords = JSON.parse($records.text());
-		    $('#my-final-table').dynatable({
-		        dataset: {
-		             records: myRecords,
-		        }
-		    });
-			
-            /*
-			var table = document.createElement("table");
-			var header = table.createTHead();
-			var row = header.insertRow();     
-			
-			row.insertCell();
-			for (j=0; j<vertices.length; j++) {
-				var cell = row.insertCell();
-				cell.innerHTML = vertices[j];
-			}
-			
-			for (i=0; i<vertices.length; i++) {
-				//var header = table.createTHead();
-				var row = table.insertRow();     
-				var cell = row.insertCell();
-				cell.innerHTML = vertices[i];
-				for (j=0; j<vertices.length; j++) {
-					var cell = row.insertCell();
-					cell.innerHTML = MatrizAdjacencia[i][j];
-				}
-			}
-
-
-			relatorio.appendChild(table); */	
-			
-		};
+ReportProfessorController = Class.extend({
 	
-		/**
-		 * Cria o cabecalho do relatório
-		 */
-		var doCarregarCabecalho = function (element){
-			/*
-			 * Titulo do Relatorio
-			 */
-			var relatorio = $element;
-			var h = $(document.createElement("H1"))
-			h.appendChild($(document.createTextNode("Relatório de Análise")));
-		    relatorio.appendChild(h);
-			/*
-			 * Aviso fornecido para informar qur o programa começa aqui
-			 */
-			var p = $(document.createElement("p"))
-			p.appendChild(document.createTextNode("Aqui temos que produzir informações compreensíveis para tutores e alunos do ensino superior."));
-		    relatorio.appendChild(p);
+	titLinha: null,
+	titColuna:null,
+	matrix:null,		
+
+	/**
+	 *  Limpa o elemento de tela informado no parametro element.
+	 *  element - elemento que será removido da tela.
+	 */
+	doClearElement: function(element){
+		$(element).empty();
+	},
+	
+	/**
+	 * Cria a matriz para montar o json.
+	 * ---------------------------------
+	 */
+	doCreateMatrizAdjacencia: function(){
+		var mockDataModel = new MockDataModel();
+		var conversas = mockDataModel.doPreparaConversas();
+		var turma = mockDataModel.getTurma();
+		var titColuna = new Array();
+		var titLinha  = new Array();
+		this.carregarLinhaColuna(turma);
+		this.doCreateMatrizResultado;
+		for (conversa in conversas){
+		    var pessoas =  conversa.split("|");
+		    this.doSomaElementos(pessoas);
 		}
-		/**
-		 * Mostra o resumo após de montado a tabela
-		 * rpm = ReportProfessorModel
-		 */
-		var doMostrarResumo =function(element,rpm){
-			var relatorio = $(element);
-			//-- Outlier values
-		    var lnSep = $(document.createElement("BR"));
-		    var oPara = $(document.createElement("p"));
-		    var inOutlierNode = 
-		    	$(document.
-		    			createTextNode("Grau de Entrada: Valor fora da curva - " + "Mínimo:" + 
-		    			rpm.getInLow() + "; " + "Máximo:" + rpm.getInHigh()));
-		 	var outOutlierNode = 
-		 		$(document.
-		 				createTextNode("Grau de Saída: Valor fora da curva - " + "Mínimo:" + 
-		 				rpm.getOutLow() + "; " + "Máximo:" + getOutHigh()));
-		    oPara.appendChild(inOutlierNode);
-		    oPara.appendChild(lnSep);
-		    oPara.appendChild(outOutlierNode);
-		    relatorio.appendChild(oPara);	
-		};
-		
-		/**
-		 * Monta descricao do resuno
-		 */
-		var doShowMensagens= function(element,rpm){
-			
+	},
+	
+    /**
+     * Soma os elementos do vetor
+     */
+	doSomaElementos: function(pessoas){
+		var myX = doCheckPosicaoVetor(titLinha,pessoas[0]);
+		var myY = doCheckPosicaoVetor(titiColuna,pessoas[1]);
+		this.matrix[myX][myY] = this.matrix[myX][myY] + 1;
+	},
+	
+	/**
+	 * Encontra o elemento na linha ou coluna
+	 */
+	doCheckPosicaoVetor: function(vetor,pessoa){
+	    for (var pos = 0; vetor.lenght; ++pos ){
+	    	if (vetor[pos]==pessoa){
+	    		return pos;
+	    	}
+	    }	
+	    return -1;
+	},
+	
+	/**
+	 * Cria a matriz zerada
+	 * ---------------------
+	 */
+	doCreateMatrizResultado: function(){
+		var tamLinha  = this.titLinha.lenght;
+		var tamColuna = this.titColuna.lenght; 
+		this.matrix = new Array(tamLinha)(tamColuna);
+		for (var l = 0; l < tamLinha; ++ l ){
+			for (var c = 0; c< tamColuna; ++c){
+				matrix[l][c] = 0;
+			}
 		}
+	},
 		
-		/**
-		 * Executa a rotina principal do projeto
-		 */
-		var doRelatarAction= function(element) {
-			var relatorio = $(element);
-			var rpm  = new ReportPorfessorModel();
-			this.doClearElement(element);
-			/* Carrega o cabecalho */
-			this.doCarregarCabecalho(elemento)
-			/* Gerar matriz Adjacencia */
-			rpm = this.doRelatarMatrizAdjacencia(element);
-			/* Apresenta resumo da MAtriz */
-			this.doMostrarResumo (element,rpm);
-			this.doShowMensagens(element,rpm);
+	/**
+	 * Monta o Vetor de Linhas e Colunas
+	 * ---------------------------------
+	 */
+	carregarVetorLinhaColuna: function(){
+		this.titColuna = new Array();
+		this.titLinha  = new Array();
+		for (participante in turma){
+			this.titLinha.push(participante);
+			this.titColuna.push(participante);
 		}
+		this.titLinha.push("Total");
+		this.titColuna.push("Total");
+	},
+	
+	/**
+	 * Cria o cabecalho do relatório
+	 */
+	doCarregarCabecalho: function (element){
+		/*
+		 * Titulo do Relatorio
+		 */
+		var relatorio = $element;
+		var h = $(document.createElement("H1"))
+		h.appendChild($(document.createTextNode("Relatório de Análise")));
+	    relatorio.appendChild(h);
+		/*
+		 * Aviso fornecido para informar qur o programa começa aqui
+		 */
+		var p = $(document.createElement("p"))
+		p.appendChild(document.createTextNode("Aqui temos que produzir informações compreensíveis para tutores e alunos do ensino superior."));
+	    relatorio.appendChild(p);
+	},
+	
+	/**
+	 * Mostra o resumo após de montado a tabela
+	 * rpm = ReportProfessorModel
+	 */
+	 doMostrarResumo: function(element,rpm){
+		var relatorio = $(element);
+		//-- Outlier values
+	    var lnSep = $(document.createElement("BR"));
+	    var oPara = $(document.createElement("p"));
+	    var inOutlierNode = 
+	    	$(document.
+	    			createTextNode("Grau de Entrada: Valor fora da curva - " + "Mínimo:" + 
+	    			rpm.getInLow() + "; " + "Máximo:" + rpm.getInHigh()));
+	 	var outOutlierNode = 
+	 		$(document.
+	 				createTextNode("Grau de Saída: Valor fora da curva - " + "Mínimo:" + 
+	 				rpm.getOutLow() + "; " + "Máximo:" + getOutHigh()));
+	    oPara.appendChild(inOutlierNode);
+	    oPara.appendChild(lnSep);
+	    oPara.appendChild(outOutlierNode);
+	    relatorio.appendChild(oPara);	
+	},
+	
+	/**
+	 * Monta descricao do resuno
+	 */
+	doShowMensagens: function(element,rpm){
 		
-		
+	},
+	
+	/**
+	 * Executa a rotina principal do projeto
+	 */
+	doMainAction: function(element) {
+		var relatorio = $(element);
+		var rpm  = new ReportPorfessorModel();
+		this.doClearElement(element);
+		/* Carrega o cabecalho */
+		this.doCarregarCabecalho(elemento)
+		/* Gerar matriz Adjacencia */
+		rpm = this.doRelatarMatrizAdjacencia(element);
+		/* Apresenta resumo da MAtriz */
+		this.doMostrarResumo (element,rpm);
+		this.doShowMensagens(element,rpm);
+	}	
+	
+	
 });
+
 	
--- WebContent/relatorios/js/model/MockDataModel.js
+++ WebContent/relatorios/js/model/MockDataModel.js
@@ -1,64 +1,75 @@
 /**
  * 
  */
-var MockDataModel = 
-	( function ($){
-		var professores = ["@Amanda","@Luciene","@Marta","@Rita"];
-		var alunos       = ["José", "Maria", "Marta","Madalena","Thiago","Lucas", 
-		                    "Mateus","João", "Rita","Barbara","Cecília", "Tatianenumero",
-		                    "Modesto","Exagerado","Descansado"];
-		var turma        = [];
-	    var conversas    = [];
-		var numAlunos	 = 0;
+
+
+MockDataModel = Class.extend({
+	professores: ["@Amanda","@Luciene","@Marta","@Rita"],
+	alunos:      ["José", "Maria", "Marta","Madalena","Thiago","Lucas", 
+	             "Mateus","João", "Rita","Barbara","Cecília", "Tatianenumero",
+	             "Modesto","Exagerado","Descansado"],
+	turma:       new Array(),
+    conversas:    new Array(),
+	numAlunos:	  0,
+	
+	/**
+	 * Carrega a turma para processar a conversa
+	 */
+	doPreparaTurma: function(numAlunos){
+	
+	},
+	
+	/**
+	 * Perara a conversa entre alunos e professor
+		 * retorna um json com as conversas
+	 */
+	doPreparaConversas: function(turma,interacoes){
+
+    },
+    
+    /**
+	 * Gera o bate papo. Chamar através desta função
+	 * @param numeroAlunos
+	 */
+	doGerarBatePapo: function(numeroAlunos, interações){
+	    var numAlunos = numeroAlunos-0;
+	    var numInteracoes  = interacoes - 0
+	    numAlunos = (numAlunos > 15)?15:nunAlunos;
+	    this.doPreparaTurma(numAlunos);
+		return this.doPreparaConversas(interacoes);
+	},
+	
+	/**
+	 * Seleciona um professor e inclui em turma
+	 */
+	getProfessorAleatorio: function(){
 		
-		/**
-		 * Carrega a turma para processar a conversa
-		 */
-		var doPreparaTurma = function(numAlunos){
-		
-		}
-		
-	    /**
-	     * Perara a conversa entre alunos e professor
-	     * retorna um json com as conversas
-	     */
-		var doPreparaConversas = function(turma,interacoes){
-		
-		}
-		
-		/**
-		 * Gera o bate papo. Chamar através desta função
-		 * @param numeroAlunos
-		 */
-		doGerarBatePapo = function(numeroAlunos, interações){
-		    var numAlunos = numeroAlunos-0;
-		    var numInteracoes  = interacoes - 0
-		    numAlunos = (numAlunos > 15)?15:nunAlunos;
-		    this.doPreparaTurma(numAlunos);
-			return this.doPreparaConversas(interacoes);
-		};
-		
-		/**
-		 * Seleciona um professor e inclui em turma
-		 */
-		var  getProfessorAleatorio=function(){
-			
-		};
-		
-		/**
-		 * Seleciona os alunos e inclui em turoma
-		 */
-		var getAlunoAleatorio=function(numAlunos){
-			if (numAlunos >= 15){
-				for (aluno in this.alunos){
-					this.turma.put[aluno];
-				}
-				return;
+	},
+
+	/**
+	 * Seleciona os alunos e inclui em turoma
+	 */
+	getAlunoAleatorio: function(numAlunos){
+		if (numAlunos >= 15){
+			for (aluno in this.alunos){
+				this.turma.put[aluno];
 			}
-			/*
-			 * gerar grupo aleatório
-			 */
-			
-		};
+			return;
+		}
+		/*
+		 * gerar grupo aleatório
+		 */
 		
-	});
+	},
+	
+
+	getTurma: function(){
+		return this.turma;
+	},
+	
+	
+});
+
+
+
+
